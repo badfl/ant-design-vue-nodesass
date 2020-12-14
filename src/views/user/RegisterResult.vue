c41b7e82397d52a2e5ed647e99ae21456615e29d
badfl <badfl@qq.com>
2020-12-14 10:26:22 +0800

    初始化项目

---
+++ src/views/user/RegisterResult.vue
@@ -0,0 +1,43 @@
+<template>
+  <a-result
+    :isSuccess="true"
+    :content="false"
+    :title="username"
+    :sub-title="description">
+
+    <template #extra>
+      <a-button size="large" style="margin-left: 8px" @click="goHomeHandle">返回登录</a-button>
+    </template>
+
+  </a-result>
+</template>
+
+<script>
+export default {
+  name: 'RegisterResult',
+  data () {
+    return {
+      description: '',
+      form: {}
+    }
+  },
+  computed: {
+    username () {
+      const v = this.form && this.form.username || 'xxx'
+      return `你的账户：${v} 注册成功`
+    }
+  },
+  created () {
+    this.form = this.$route.params
+  },
+  methods: {
+    goHomeHandle () {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
